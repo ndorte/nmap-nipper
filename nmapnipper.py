d37d91e6f700e980fbba0922a9f440c956dc172a
Uğur Kubilay ÇAM <32200607+ndorte@users.noreply.github.com>
2018-10-19 12:12:13 +0300

    Update nmapnipper.py

--- nmapnipper.py
+++ nmapnipper.py
@@ -183,7 +183,7 @@ e.g. sudo python3 nmapnipper.py -i 192.168.1.1 -p 1-8888
 advanced nipper: sudo python3 nmapnipper.py -i [ipadress or ip range] -p [port or port range] -n [nmap options] -l [ip:port list] -r [save result list]
 e.g. sudo python3 nmapnipper.py -i 192.168.1.1-255 -p 1-8888 -n "-sS -sV -T4" -l list.txt -r result.txt
 
-scan nmap xml report: sudo python3 nmapripper.py -x [xmlfile]
-e.g. sudo python3 nmapripper.py -x nmapReport.xml
+nmap xml report nipper: sudo python3 nmapripper.py -x [xmlfile]
+e.g. sudo python3 nmapnipper.py -x nmapReport.xml
     
     ''')
